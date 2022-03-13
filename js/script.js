649cff81a437918f1f3d0372dcb80c349687bf97
Agnes-Kalunda <agnes.mbiti@student.moringaschool.com>
2022-03-13 22:41:38 +0300

    declared a variable in js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var textOverImages=document.querySelectorAll(".onClickTextOverImage div");
+
+for(var i=o; i<textOverImages.length; i++){
+    textOverImages[i].onclick=function(){
+        this.classList.toggle("show");
+    }
+}
