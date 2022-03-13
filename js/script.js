71244bdcfd008dae0680fea9fbec1a9ec90f5f74
Agnes-Kalunda <agnes.mbiti@student.moringaschool.com>
2022-03-14 01:11:49 +0300

    edited my js file

--- js/script.js
+++ js/script.js
@@ -1,7 +1,53 @@
-var textOverImages=document.querySelectorAll(".onClickTextOverImage div");
 
-for(var i=o; i<textOverImages.length; i++){
-    textOverImages[i].onclick=function(){
-        this.classList.toggle("show"); 
-    }
-}
+$(document).ready(function () {
+    $(".img_1").click(function () {
+      $(this).toggle(function () {
+        $(".design").toggle();
+      });
+    });
+    $(".design").click(function () {
+      $(this).toggle(function () {
+        $(".img_1").toggle();
+      });
+    });
+  });
+  
+  $(document).ready(function () {
+    $(".img_2").click(function () {
+      $(this).toggle(function () {
+        $(".development").toggle();
+      });
+    });
+    $(".development").click(function () {
+      $(this).toggle(function () {
+        
+  
+
+
+
+
+// toggle effect
+// $(document).ready(function () {
+//     $("#imgDesign").click(function () {
+//       $(this).toggle(function () {
+//         $("#pDesign").toggle();
+//       });
+//     });
+//     $("#development").click(function () {
+//       $(this).toggle(function () {
+//         $("#pImgDesign").toggle();
+//       });
+//     });
+//   });
+  
+//   $(document).ready(function () {
+//     $("#imgProduct").click(function () {
+//       $(this).toggle(function () {
+//         $("#prodMan").toggle();
+//       });
+//     });
+
+  
+
+
+        
