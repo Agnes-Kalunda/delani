f69ecf7a84da5d3bbf69ccca35ec12812398d654
Agnes-Kalunda <agnes.mbiti@student.moringaschool.com>
2022-03-14 15:49:27 +0300

    added a mouseover function in my js

--- js/script.js
+++ js/script.js
@@ -31,23 +31,50 @@ $(".pIconC").click(function(){
  $(".pIconC").toggle();
  $(".iconC").toggle();
 }) 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 })
+
+
+// $('.img1').mouseenter(function(){
+//     $('.img1Name').filter('#img1Name1').show("show");
+// }).mouseleave(function(){
+//     $('#img1Name1').hide("show");
+// })
+
+
+$('.imageOne').mouseover(function(){
+    $('#img1P').show();
+}) .mouseout(function(){
+    $('#img1P').hide();
+})
+
+
+$('.imageTwo').mouseover(function(){
+    $('#img2P').show();
+}) .mouseout(function(){
+    $('#img2P').hide();
+})
+
+
+$('.imageThree').mouseover(function(){
+    $('#img3P').show();
+}) .mouseout(function(){
+    $('#img3P').hide();
+})
+
+
+$('.imageFour').mouseover(function(){
+    $('#img4P').show();
+}) .mouseout(function(){
+    $('#img4P').hide();
+})
+
+
+
+
+
+
+
+
 
 
 
