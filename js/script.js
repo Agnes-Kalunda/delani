20ec9867c6aac8c6fd0253cbf815e0c2feca294f
Agnes-Kalunda <agnes.mbiti@student.moringaschool.com>
2022-03-16 22:12:00 +0300

    edited my form

--- js/script.js
+++ js/script.js
@@ -98,7 +98,20 @@ $('.imageEight').mouseover(function(){
 })
 
 
-
+$(document).ready(function() {
+    $("form#formm").submit(function(event) {
+        var name = $("input#name").val();
+        var email = $("input#email").val();
+        var message = $("").val();
+        if ($("input#name").val() && $("input#email").val()) {
+            alert(name + ", we have received your message. Thank you for reaching out to us.");
+        } else {
+            alert("Please enter your Name and Email");
+        }
+    });
+});
+function myFunction() {
+}
 
 
 
